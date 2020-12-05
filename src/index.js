f562a5d08ff927eccee1528a2cc447481b0176a1
吴贻韡 <mizuka.wu@outlook.com>
2020-12-06 00:34:28 +0800

    fix: stream

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Vmd from './Vmd.js'
 
 export { default as Vmd } from './Vmd'
-export { default as ReadBufferStream } from './ReadBufferStream'
-export { default as WriteBufferStream } from './WriteBufferStream'
+export { default as ReadBufferStream } from './stream/ReadBufferStream'
+export { default as WriteBufferStream } from './stream/WriteBufferStream'
 
 export default Vmd
