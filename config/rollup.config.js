e4bc6121639e986372ec564f7aad34d16d9f6d0e
吴贻韡 <mizuka.wu@outlook.com>
2020-12-06 01:00:50 +0800

    feat: 更新打包工具

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,9 +1,32 @@
+import resolve from '@rollup/plugin-node-resolve'
+import common from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+// import buble from '@rollup/plugin-buble'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+import { terser } from 'rollup-plugin-terser'
+
+const output = {
+  name: 'Vmd',
+  file: 'dist/vmd.js',
+  format: 'umd',
+  exports: 'named'
+}
+
 export default {
   input: 'src/index.js',
-  output: {
-    name: 'Vmd',
-    file: 'dist/vmd.js',
-    format: 'umd',
-    external: ['shiftjis']
-  }
+  output: [
+    output,
+    {
+      ...output,
+      file: 'dist/vmd.min.js',
+      plugins: [terser()]
+    }
+  ],
+  plugins: [
+    nodePolyfills(),
+    json(),
+    common(),
+    resolve()
+    // buble()
+  ]
 }
